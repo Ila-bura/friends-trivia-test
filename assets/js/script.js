faffd4df849921a95d32416a8d644065cba46050
Ila-bura <>
2023-05-20 10:05:11 +0000

    Add quiz questions and possible answers

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+// Array with quiz questions
+
+let allQuestions = {
+    'How many sisters does Joey have?' :
+    ['7', '6', '5'],
+
+    'What dessert did Rachel try to make for Thanksgiving?' :
+    ['Toffee', 'Truffle', 'Trifle'],
+
+    'What is the title of Phoebe\'s most popular song?' :
+    ['Stinky Cat', 'Smelly Cat', 'Silly Cat'],
+
+    'What is a lobster?' :
+    ['Your gym buddy', 'Your favourite roommate', 'The person you are meant to be with'],
+
+    'What is the name of Ross\'s monkey?' :
+    ['Marcel', 'Miguel', 'Manuel'],
+
+    'What job does Chandler\'s dad do in Vegas?' :
+    ['A croupier', 'Elvis impersonator', 'A drag queen'],
+
+    'What did Phoebe legally change her name to?' :
+    ['Regina Phalange', 'Princess Consuela Banana Hammock', 'Ursula Muriel'],
+
+    'Which of these celebrities did not make a cameo in the series?' :
+    ['Robin Williams', 'Isabella Rossellini', 'Cher'],
+
+    'What Japanese product does Joey do a commercial for?' :
+    ['A shower cap for men', 'A lipstick for men', 'A handbag for men'],
+
+   'What are the names of Monica and Ross\' parents?' :
+   ['Judy and Jack', 'Rose and Ronnie', 'Pam and Pete'],
+}
+
+
+// Function to start the quiz
+
+
+// Function to display the questions
+
+
+// Function to check if answer is correct 
+
+// Function to clear the area
+
+// Function to display the final score
+
+
+// Function to restart the quiz
+
